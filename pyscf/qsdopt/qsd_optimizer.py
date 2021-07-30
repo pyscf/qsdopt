1183b0397aac897707ff4e179154bb5b1d9cb6a2
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2021-07-30 22:57:25 +0200

    add qsd_step function

--- pyscf/qsdopt/qsd_optimizer.py
+++ pyscf/qsdopt/qsd_optimizer.py
@@ -12,8 +12,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import numpy as np
+
 from pyscf import lib
 from pyscf.grad.rhf import GradientsMixin
+
+
+def qsd_step(x0, g, H, sm3, stationary_point, step=1e-1):
+    zero_thres = 1e-10
+    step_thres = 1e-1 * step
+    eigval, eigvec = np.linalg.eigh(H)
+    NMeigvec = eigvec.T[np.abs(eigval) > zero_thres]
+    hNM = eigval[np.abs(eigval) > zero_thres]
+    gNM = NMeigvec @ g
+    delta = gNM / hNM
+    if stationary_point == "TS":
+        hNM[0] = -hNM[0]
+
+    inc = NMeigvec.T @ delta / sm3
+    if np.linalg.norm(inc) < step:
+        return -inc
+
+    if all(hNM > 0):
+        umin = 0e0
+    else:
+        umin = 1e-25
+
+    incNM = (umin ** hNM - 1e0) * delta
+    inc = NMeigvec.T @ incNM / sm3
+    inc_norm = np.linalg.norm(inc)
+
+    if inc_norm > step:
+        # Search u value.
+        umax = 1e0
+        while np.abs(inc_norm - step) > step_thres:
+            u = (umax + umin) / 2.0
+            incNM = (u ** hNM - 1e0) * delta
+            inc = NMeigvec.T @ incNM / sm3
+            inc_norm = np.linalg.norm(inc)
+            if inc_norm < step:
+                umax = u
+            else:
+                umin = u
+    print("u, max norm, real inc", umin, u, np.linalg.norm(delta), np.linalg.norm(inc))
+    return inc
 
 
 class QSD(lib.StreamObject):
