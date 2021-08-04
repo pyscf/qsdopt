bbb88a1fdec94fc36dcbf8140d1d44eceda33bec
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2021-08-04 16:22:31 +0200

    add uthreshold

--- pyscf/qsdopt/qsd_optimizer.py
+++ pyscf/qsdopt/qsd_optimizer.py
@@ -84,6 +84,7 @@ def kernel(g_scanner, stationary_point, hess_update_rule, hess_update_freq=5):
 def qsd_step(x0, g, H, sm3, stationary_point, step=1e-1):
     zero_thres = 1e-10
     step_thres = 1e-1 * step
+    uthres = 1e-320
     eigval, eigvec = np.linalg.eigh(H)
     NMeigvec = eigvec.T[np.abs(eigval) > zero_thres]
     hNM = eigval[np.abs(eigval) > zero_thres]
@@ -108,7 +109,7 @@ def qsd_step(x0, g, H, sm3, stationary_point, step=1e-1):
     if inc_norm > step:
         # Search u value.
         umax = 1e0
-        while np.abs(inc_norm - step) > step_thres:
+        while np.abs(inc_norm - step) > step_thres and umax > uthres:
             u = (umax + umin) / 2.0
             incNM = (u ** hNM - 1e0) * delta
             inc = NMeigvec.T @ incNM / sm3
