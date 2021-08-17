824cb0f72bfcc1da19bd27d454993d62bfae3060
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2021-08-17 13:52:28 +0200

    add H2O min test

--- pyscf/test/test_qsdopt.py
+++ pyscf/test/test_qsdopt.py
@@ -22,3 +22,18 @@ class QSDOptTests(unittest.TestCase):
         assert_almost_equal(d1, d2, 4)
         assert_almost_equal(phi, np.pi, 4)
         self.assertTrue(optimizer.converged)
+
+    def test_H2O_min(self):
+        mol = gto.M(atom="O 0 0 0; H 0 0 1.2; H 0, 0.5, -1.2", basis="minao", verbose=0)
+        mf = scf.RHF(mol)
+        optimizer = QSD(mf, stationary_point="min")
+        optimizer.kernel()
+        coords = mol.atom_coords()
+        v1 = coords[0, :] - coords[1, :]
+        v2 = coords[0, :] - coords[2, :]
+        d1 = np.linalg.norm(v1)
+        d2 = np.linalg.norm(v2)
+        phi = np.arccos(v1.dot(v2) / (d1 * d2))
+        assert_almost_equal(d1, d2, 4)
+        assert_almost_equal(phi, np.radians(104.3), 4)
+        self.assertTrue(optimizer.converged)
